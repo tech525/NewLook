b383b475de07385b4e86bf05f69ef7f1bf464dd8
tech525 <techte09@gmail.com>
2024-12-26 17:32:27 +0000

    Add files via upload

--- NewLookScript.py
+++ NewLookScript.py
@@ -162,6 +162,5 @@ def index():
     '''
 
 if __name__ == '__main__':
-    import os
     port = int(os.environ.get("PORT", 5000))  # Use Render's PORT environment variable
     app.run(host='0.0.0.0', port=port, debug=True)
